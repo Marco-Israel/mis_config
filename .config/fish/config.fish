da6a1018d7290a8db5ec81cdfe47a18ed06fb992
Marco-Israel <Marco-Israel@gmx.de>
2021-06-21 11:41:34 +0000

    WIP NB

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,47 @@
 fish_vi_key_bindings
 
+#Remove fish default greeting
+set -U fish_greeting
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
+
+
+
+# Launch debugger on SIGTRAP
+#
+function fish_sigtrap_handler --on-signal TRAP --no-scope-shadowing --description "Signal handler for the TRAP signal. Launches a debug prompt."
+    breakpoint
+end
+
+
+# Allow %n job expansion to be used with fg/bg/wait
+# `jobs` is the only one that natively supports job expansion
+function __fish_expand_pid_args
+    for arg in $argv
+        if string match -qr '^%\d+$' -- $arg
+            if not jobs -p $arg
+                return 1
+            end
+        else
+            printf "%s\n" $arg
+        end
+    end
+end
+
+for jobbltn in bg wait disown
+    function $jobbltn -V jobbltn
+        builtin $jobbltn (__fish_expand_pid_args $argv)
+    end
+end
+function fg
+    builtin fg (__fish_expand_pid_args $argv)[-1]
+end
+
+function kill
+    command kill (__fish_expand_pid_args $argv)
+end
